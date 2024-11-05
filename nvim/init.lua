cae4fc05dea6f8a7954390473b7dddd4a29c6731
JWSchaefer <joe.w.schaefer@gmail.com>
2024-11-05 17:39:06 +0000

    Rolling changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,20 +2,18 @@ require("JWSchaefer.config.lazy")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    spec = {
-      -- import your plugins
-      { import = "JWSchaefer.plugins" }, 
-      { import = "JWSchaefer.plugins.lsp" },
-    },
-    -- Configure any other settings here. See the documentation for more details.
-    -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "iceburg" } },
-    -- automatically check for plugin updates
-    checker = { enabled = true },
-  })
+	spec = {
+		-- import your plugins
+		{ import = "JWSchaefer.plugins" },
+		{ import = "JWSchaefer.plugins.lsp" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "iceburg" } },
+	-- automatically check for plugin updates
+	checker = { enabled = false },
+})
 
 require("JWSchaefer.themeing")
 require("JWSchaefer.keymaps")
 require("JWSchaefer.options")
-
-
--- nvim/lua/JWSchaefer/keymaps.lua
+++ nvim/lua/JWSchaefer/keymaps.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = " "
 
-
-
 -- Telescope
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
@@ -9,28 +7,41 @@ vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live gr
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
+-- Comments
+vim.keymap.set("x", "<Leader>/", "<plug>(comment_toggle_linewise_visual)")
+
 -- Neotree
-vim.api.nvim_set_keymap('n', '<leader><Enter>', ':Neotree toggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader><Enter>", ":Neotree toggle<CR>", { noremap = true, silent = true })
 
 local map = vim.keymap.set
-local silent = { silent = true }
+local opts = { silent = true, noremap = true }
 
 -- basic
-map("n", "<Leader>w", "<Cmd>w<CR>", silent)
-map("n", "<Leader>q", "<Cmd>q<CR>", silent)
+map("n", "<Leader>w", "<Cmd>w<CR>", opts)
+map("n", "<Leader>q", "<Cmd>q<CR>", opts)
 
-map({ "n", "i" }, "<D-Up>", ":10+<CR>", silent)
-map({ "n", "i" }, "<D-Down>", ":10-<CR>", silent)
+map({ "n", "i" }, "<D-Up>", "<Cmd>10+<CR>", opts)
+map({ "n", "i" }, "<D-Down>", "<Cmd>10-<CR>", opts)
 
 -- select all map("n", "<C-a>", "ggVG", silent)
 -- delete words
-map("i", "<C-BS>", "<C-w>", silent)
-map("i", "<C-Del>", "<C-o>dw", silent)
+map("i", "<C-BS>", "<C-w>", opts)
+map("i", "<C-Del>", "<C-o>dw", opts)
 
 -- move between words in insert mode
-map("i", "<C-Left>", "<C-o>b", silent)
-map("i", "<C-Right>", "<C-o>e", silent)
+map("i", "<C-Left>", "<C-o>b", opts)
+map("i", "<C-Right>", "<C-o>e", opts)
 
 -- -- git
-map("n", "<Leader>vd", "<Cmd>Gvdiffsplit!<CR>", silent)
+map("n", "<Leader>vd", "<Cmd>Gvdiffsplit!<CR>", opts)
 
+-- Window
+
+vim.api.nvim_set_keymap("n", "<C-Left>", "<C-w>h", opts)
+vim.api.nvim_set_keymap("n", "<C-Right>", "<C-w>l", opts)
+vim.api.nvim_set_keymap("n", "<C-Up>", "<C-w>k", opts)
+vim.api.nvim_set_keymap("n", "<C-Down>", "<C-w>j", opts)
+
+vim.keymap.set("n", "<leader>gl", function()
+	vim.diagnostic.open_float()
+end, { desc = "Trigger Linting for current file" })
